fbcdb95dd4bf6736816704dd21ac3843899dbbea
Robert Freeman <robbie.freeman92@icloud.com>
2022-02-26 13:54:03 -0500

    created the migration file that defines the "movies_theaters" table

---
+++ src/db/migrations/20220226134311_createMoviesTheatersTable.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('movies_theaters', table => {
+    table.integers('movie_id');
+    table
+      .foreign('movie_id')
+      .references('movie_id')
+      .inTable('movies');
+    table.integers('theater_id');
+    table
+      .foreign('theater_id')
+      .references('theater_id')
+      .inTable('theaters');
+    table.boolean('is_showing');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('movies_theaters');
+};
